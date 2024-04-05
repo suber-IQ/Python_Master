f352b117e8ac978aa9fdf9559521ba7e4c86f40a
sumit <suberquercusumit@gmail.com>
2024-04-05 22:29:02 +0530

    fix error

--- Stage1/14Tuple/02FunctionTuple/functionTuple.py
+++ Stage1/14Tuple/02FunctionTuple/functionTuple.py
@@ -4,17 +4,18 @@
 
 t = (20,30,40,50,20)
 
-m = min(t)
-print(m) # 🎉 output:- 20
-m = max(t)
-print(m) #  🎉 output:-50
-c = t.count(20) 
-print(c) #  🎉 output:- 2
-i = t.index(50)
-print(i) #  🎉 output:- 3 (if not found return error)
-s = sum(t)
-print(s) #  🎉 output:- 160
-s = sum(t,10)
-print(s)  #  🎉 output:- 160 + 10 = 170
+# m = min(t)
+# print(m) # 🎉 output:- 20
+# m = max(t)
+# print(m) #  🎉 output:-50
+# c = t.count(20) 
+# print(c) #  🎉 output:- 2
+# i = t.index(50)
+# print(i) #  🎉 output:- 3 (if not found return error)
+# s = sum(t)
+# print(s) #  🎉 output:- 160
+# s = sum(t,10)
+# print(s)  #  🎉 output:- 160 + 10 = 170
 
+# The end 
 
